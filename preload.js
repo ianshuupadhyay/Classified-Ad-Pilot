a1b7fa2502dde1c7b3b42479f4d33beea17f21fa
ianshuupadhyay <anshuku34@gmail.com>
2025-05-07 01:26:31 +0530

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,7 @@
+// Preload script
+// This file is needed because we're using contextIsolation: false
+// It's kept minimal for now but can be expanded if needed
+
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('Preload script loaded');
+  });
